57d14c62ca09e192aa11e55faa2f1cca7e6b76b9
oojas <oojasgupta@gmail.com>
2020-09-28 03:28:35 +0530

    added appbar

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("What do you want to learn"),
+      ),
+    );
   }
 }
