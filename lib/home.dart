c1e698b3a8671408f0673a7c155401de40c219ea
oojas <oojasgupta@gmail.com>
2020-09-28 03:30:13 +0530

    updating appbar

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("What do you want to learn"),
+        title: Text(
+          "What do you want to learn",
+          style: GoogleFonts.acme(fontSize: 21, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
