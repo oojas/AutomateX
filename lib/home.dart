3c1b16a3b4543226b4932e2d5622d716ac66eb53
oojas <oojasgupta@gmail.com>
2020-09-30 01:44:32 +0530

    adding animations

--- lib/home.dart
+++ lib/home.dart
@@ -12,14 +12,17 @@ class _homeState extends State<home> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     _controller =
-        new AnimationController(duration: Duration(seconds: 3), vsync: this);
-    _animation = new Tween(begin: 0.0, end: 300.0).animate(_controller);
+        new AnimationController(duration: Duration(seconds: 3), vsync: this,
+        
+        );
+    _animation = new Tween(begin: 0.0, end: 300.0).animate(CurvedAnimation(parent: _controller, curve:Curves.bounceInOut);
     super.initState();
+    _controller.forward();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    
     _controller.dispose();
     super.dispose();
   }
@@ -27,85 +30,161 @@ class _homeState extends State<home> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 15.0,
-          automaticallyImplyLeading: true,
-          leading: Icon(
-            Icons.archive,
-            color: Colors.cyanAccent,
-          ),
-          title: Text(
-            "What do you wanna Learn?",
-            style: GoogleFonts.permanentMarker(
-                fontSize: 21,
-                fontStyle: FontStyle.normal,
-                color: Colors.amberAccent),
-          ),
+      appBar: AppBar(
+        elevation: 15.0,
+        automaticallyImplyLeading: true,
+        leading: Icon(
+          Icons.question_answer,
+          color: Colors.yellow,
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: FloatingActionButton(
-                onPressed: null,
-                splashColor: Colors.blue,
-                backgroundColor: Colors.amber,
-                child: Icon(Icons.speaker_notes),
-              ),
+        title: Text(
+          "What do you wanna Learn?",
+          style: GoogleFonts.permanentMarker(
+              fontSize: 21,
+              fontStyle: FontStyle.normal,
+              color: Colors.amberAccent),
+        ),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.functions,
+                  color: Colors.white70,
+                ),
+                trailing: Icon(
+                  Icons.info_outline,
+                  color: Colors.indigoAccent,
+                ),
+                title: Text(
+                  "DFA",
+                  style: GoogleFonts.courgette(
+                      fontSize: 21, color: Colors.redAccent),
+                ),
+                dense: true,
+                selected: true,
+                focusColor: Colors.pinkAccent,
+                hoverColor: Colors.pink,
+                contentPadding: EdgeInsets.all(12.0),
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.functions,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "NFA",
+                  style: GoogleFonts.courgette(
+                      fontSize: 21, color: Colors.redAccent),
+                ),
+                dense: true,
+                selected: true,
+                focusColor: Colors.pinkAccent,
+                hoverColor: Colors.pink,
+                contentPadding: EdgeInsets.all(12.0),
+                trailing: Icon(
+                  Icons.info_outline,
+                  color: Colors.indigoAccent,
+                ),
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.functions,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "NFA --> DFA",
+                  style: GoogleFonts.courgette(
+                      fontSize: 21, color: Colors.redAccent),
+                ),
+                dense: true,
+                selected: true,
+                focusColor: Colors.pinkAccent,
+                hoverColor: Colors.pink,
+                contentPadding: EdgeInsets.all(12.0),
+                trailing: Icon(
+                  Icons.info_outline,
+                  color: Colors.indigoAccent,
+                ),
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.functions,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "Minimization of DFA",
+                  style: GoogleFonts.courgette(
+                      fontSize: 21, color: Colors.redAccent),
+                ),
+                dense: true,
+                selected: true,
+                focusColor: Colors.pinkAccent,
+                hoverColor: Colors.pink,
+                contentPadding: EdgeInsets.all(12.0),
+                trailing: Icon(
+                  Icons.info_outline,
+                  color: Colors.indigoAccent,
+                ),
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.functions,
+                  color: Colors.white70,
+                ),
+                title: Text(
+                  "CNF | GNF | CFG",
+                  style: GoogleFonts.courgette(
+                      fontSize: 21, color: Colors.redAccent),
+                ),
+                dense: true,
+                selected: true,
+                focusColor: Colors.pinkAccent,
+                hoverColor: Colors.pink,
+                contentPadding: EdgeInsets.all(12.0),
+                trailing: Icon(
+                  Icons.info_outline,
+                  color: Colors.indigoAccent,
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 300.0, top: 100),
+            child: FloatingActionButton(
+              onPressed: null,
+              splashColor: Colors.blue,
+              backgroundColor: Colors.amber,
+              child: Icon(Icons.speaker_notes),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                ListTile(
-                  leading: Icon(
-                    Icons.functions,
-                    color: Colors.white70,
-                  ),
-                  trailing: Icon(
-                    Icons.info_outline,
-                    color: Colors.indigoAccent,
-                  ),
-                  title: Text(
-                    "DFA",
-                    style: GoogleFonts.courgette(
-                        fontSize: 21, color: Colors.redAccent),
-                  ),
-                  dense: true,
-                  selected: true,
-                  focusColor: Colors.pinkAccent,
-                  hoverColor: Colors.pink,
-                  contentPadding: EdgeInsets.all(12.0),
-                )
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ListTile(
-                  leading: Icon(
-                    Icons.functions,
-                    color: Colors.white70,
-                  ),
-                  title: Text(
-                    "NFA",
-                    style: GoogleFonts.courgette(
-                        fontSize: 21, color: Colors.redAccent),
-                  ),
-                  dense: true,
-                  selected: true,
-                  focusColor: Colors.pinkAccent,
-                  hoverColor: Colors.pink,
-                  contentPadding: EdgeInsets.all(12.0),
-                  trailing: Icon(
-                    Icons.info_outline,
-                    color: Colors.indigoAccent,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
