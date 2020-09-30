96a58979f19d0f401a61ad4b917a09b1ab94d44c
oojas <oojasgupta@gmail.com>
2020-09-30 18:29:36 +0530

    added Stateful widget

--- lib/Pages/DFA.dart
+++ lib/Pages/DFA.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class DFA extends StatelessWidget {
+class DFA extends StatefulWidget {
+  @override
+  _DFAState createState() => _DFAState();
+}
+
+class _DFAState extends State<DFA> {
   @override
   Widget build(BuildContext context) {
     return Container();
